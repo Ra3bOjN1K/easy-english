26b5db785eadf17300578e0c0c5e95a7cf2a3a0a
Ra3bOjN1K <Ra3bOjN1K@mail.ru>
2016-02-03 17:31:38 +0300

    added ForeignWordTypeField for model

---
+++ easy_english/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'easy_english.apps.EasyEnglishConfig'
--- easy_english/models.py
+++ easy_english/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from easy_english.model_fields import ForeignWordTypeField
+
+
+class ForeignWord(models.Model):
+    title = models.CharField(max_length=240)
+    type = ForeignWordTypeField(
+        max_length=1, choices=((0, 'Unknown'), (1, 'Verb'), (2, 'Noun')))
+
+
+class Translation(models.Model):
+    pass
+
+
+class WordAssociationPicture(models.Model):
+    pass
+
+
+class WordPronunciation(models.Model):
+    pass
